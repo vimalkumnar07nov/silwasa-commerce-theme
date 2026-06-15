a9df17cd763b813fa529b3de7899770d4723974c
vimalkumnar07nov <vimalkumar07nov@gmail.com>
2026-06-15 19:35:59 +0530

    Setup global design system and project structure

--- front-page.php
+++ front-page.php
@@ -1,1 +1,25 @@
-<?php get_header(); get_template_part('template-parts/homepage/hero'); get_footer();
+<?php
+
+get_header();
+
+?>
+
+<div class="max-w-7xl mx-auto py-20">
+
+    <h1 class="text-5xl font-bold text-green-600">
+
+        Silwasa Commerce Theme
+
+    </h1>
+
+    <p class="mt-4 text-xl">
+
+        Tailwind CSS Working Successfully
+
+    </p>
+
+</div>
+
+<?php
+
+get_footer();
